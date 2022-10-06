a0b43f2c3843240e15cbd39005611e5711219632
marsnebulasoup <marsnebulasoup@gmail.com>
2022-10-06 17:37:42 -0500

    Created stack interface

--- stack.h
+++ stack.h
@@ -1,40 +1,39 @@
-/*
- * Your comment header here
- * modify this file as needed to complete the assignment
- */
+/*********************
+Name: Kevin Bryniak
+Assignment: Int Stack
+A simple implementation of a data stack.
+**********************/
 
 #ifndef STACK_H
 #define STACK_H
 
-/*
- * there are no other includes here because they are not needed.
- * if you do need other things included, they would go here
- */
-
 #include "data.h"
+#include <string>
+#include <iostream> // DEBUG ONLY
 
-class Stack {
+using std::string;
+
+class Stack
+{
 
 public:
+  Stack(int);
+  ~Stack();
 
-    /* 
-     * write all your public method prototypes here
-     * this includes you constructor and destructor
-     */
-    
+  bool isEmpty();
+  bool push(int, string&);
+  void pop(Data&);
+  void peek(Data&);
+
+  // DEBUG ONLY
+  void dumpStack();
+
 private:
-
-    /* 
-     * write all your private method prototypes here
-     */
-
-    // these are the only attributes you need.
-    // do not modify them or add any other attributes
-    int top;
-    int size;
-    Data **stack;
-    // notice stack is a pointer to a pointer. That means if you do
-    // a single array allocation, you will get an array of pointers
+  int top;
+  int size;
+  Data **stack;
+  // notice stack is a pointer to a pointer. That means if you do
+  // a single array allocation, you will get an array of pointers
 };
 
-#endif //STACK_H
+#endif // STACK_H
