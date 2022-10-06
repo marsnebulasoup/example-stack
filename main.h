b9455627da86c27b8b98424a7353f9dab9627447
marsnebulasoup <marsnebulasoup@gmail.com>
2022-10-06 17:50:24 -0500

    Cleaned out main()

--- main.h
+++ main.h
@@ -1,23 +1,21 @@
-/*
- * your header here
- * 
- */
+/*********************
+Name: Kevin Bryniak
+Assignment: Int Stack
+A simple implementation of a data stack.
+**********************/
 
 #ifndef STACKS_MAIN_H
 #define STACKS_MAIN_H
 
-// you probably need all these,
-// but if you don't get rid of what you don't need
 #include <stdlib.h>   /* srand, rand */
 #include <time.h>     /* time */
 #include <iostream>   /* cout, endl */
 #include <string>     /* string */
 #include "functions.h"
 
-/*
- * additional directives here
- * 
- */
+using std::string;
+using std::cin;
+using std::cout;
 
 
 #endif /* STACKS_MAIN_H */
