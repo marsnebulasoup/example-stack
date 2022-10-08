4047faf10a455c45a0d6d48f8bd162be843d7939
marsnebulasoup <marsnebulasoup@gmail.com>
2022-10-07 21:40:28 -0500

    Improved rand_string readability

--- functions.cpp
+++ functions.cpp
@@ -7,14 +7,13 @@
 
 string rand_string(){
     // create size 0 to MAXSTRSIZE-1
-    int rnum = rand() % MAXSTRSIZE;
+    int length = (rand() % MAXSTRSIZE);
     
     // make buffer to hold rand string
     string str = "";
 
     // fill buffer with random characters A to Z
-    int i = 0;
-    for(; i < rnum; i++){
+    for(int i = 0; i < length; i++){
         str += (rand() % ('Z' - 'A' + 1)) + 'A';
     }    
 
